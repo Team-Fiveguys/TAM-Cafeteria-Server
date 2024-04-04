734db2ed6b71348fbe3ddafc59f0408a64d0a186
wuseong <jws001228@naver.com>
2024-04-04 18:08:28 +0900

    feat: first commit

---
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/TomCafeteriaServerApplication.java
@@ -0,0 +1,13 @@
+package fiveguys.Tom.Cafeteria.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomCafeteriaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomCafeteriaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiveguys/Tom/Cafeteria/Server/TomCafeteriaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fiveguys.Tom.Cafeteria.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomCafeteriaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
