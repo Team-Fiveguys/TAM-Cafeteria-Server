604144e3acca3e00afa33a66c6968354eff8d323
Harang Kim <berix0218@naver.com>
2024-04-08 17:33:51 +0900

    git test

--- src/main/java/fiveguys/Tom/Cafeteria/Server/TomCafeteriaServerApplication.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/TomCafeteriaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TomCafeteriaServerApplication {
-
+	//ㅎㅇ
 	public static void main(String[] args) {
 		SpringApplication.run(TomCafeteriaServerApplication.class, args);
 	}
