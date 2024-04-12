50b95a95da0de0f519b8b15f9a0115ddcf8f4c1c
wuseong <jws001228@naver.com>
2024-04-13 02:38:52 +0900

    fix: 식당 등록 API 수정

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/cafeteria/dto/response/CafeteriaCreateResponseDTO.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/cafeteria/dto/response/CafeteriaCreateResponseDTO.java
@@ -3,10 +3,13 @@ package fiveguys.Tom.Cafeteria.Server.domain.cafeteria.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class CafeteriaCreateResponseDTO {
-
+    private Long cafeteriaId;
 }
