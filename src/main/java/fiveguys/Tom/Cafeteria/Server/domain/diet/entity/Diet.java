135a6f4fe1790f8a756fb61d153f9250e984f695
wuseong <jws001228@naver.com>
2024-04-15 04:22:40 +0900

    feat: 식단 이미지 양방향 매핑, 식당 setter 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/entity/Diet.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/entity/Diet.java
@@ -1,6 +1,7 @@
 package fiveguys.Tom.Cafeteria.Server.domain.diet.entity;
 
 import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
+import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.entity.DietPhoto;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -25,12 +26,17 @@ public class Diet {
     @Enumerated(EnumType.STRING)
     private DayOfWeek dayOfWeek; // 나중에 과거의 식단까지 볼 수 있도록 확장하면 날짜로 수정할 예정
 
-    private String image; //이미지 테이블 만들어야 함
-
     @JoinColumn(name = "cafeteria_id")
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Cafeteria cafeteria;
 
     @OneToMany(mappedBy = "diet", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<MenuDiet> menuDietList;
+
+    @OneToOne(mappedBy = "diet", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private DietPhoto dietPhoto;
+
+    public void setCafeteria(Cafeteria cafeteria) {
+        this.cafeteria = cafeteria;
+    }
 }
