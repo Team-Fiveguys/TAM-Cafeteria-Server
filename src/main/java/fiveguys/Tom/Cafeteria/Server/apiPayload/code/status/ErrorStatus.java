22e57a3d5cd350b1e7c492ea90d0cf821a42daae
wuseong <jws001228@naver.com>
2024-04-18 21:06:05 +0900

    feat: 로그인 관련 에러 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/apiPayload/code/status/ErrorStatus.java
@@ -36,7 +36,15 @@ public enum ErrorStatus implements BaseCode {
     CAFETERIA_NOT_FOUND(HttpStatus.NOT_FOUND, "CAFETERIA4001", "해당 식당 id가 존재하지 않습니다."),
     CAFETERIA_IS_EMPTY(HttpStatus.BAD_REQUEST, "CAFETERIA4002", "식당은 null일 수 없습니다."),
     CAFETERIA_IS_BLANK(HttpStatus.BAD_REQUEST, "CAFETERIA4003", "식당은 공백일 수 없습니다."),
-    CAFETERIA_TOO_LONG(HttpStatus.BAD_REQUEST, "CAFETERIA4004", "해당 식당의 길이가 100을 넘어갑니다.");
+    CAFETERIA_TOO_LONG(HttpStatus.BAD_REQUEST, "CAFETERIA4004", "해당 식당의 길이가 100을 넘어갑니다."),
+
+    // 로그인 관련 에러
+    INVALID_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "LOGIN4001", "토큰값이 잘못되었거나 유효하지 않습니다."),
+    INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "LOGIN4002", "필수인자가 포함되지 않거나 인자값의 데이터 타입 또는 범위가 적절하지 않습니다."),
+    INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "LOGIN4003", "추가로 입력한 업무분야나 연차의 형식이 올바르지 않습니다."),
+    TEMPORARY_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR , "LOGIN5000", "카카오 플랫폼 서비스의 일시적 내부 장애입니다. 잠시후에 다시 시도해주세요");
+
+
 
 
     private final HttpStatus httpStatus;
