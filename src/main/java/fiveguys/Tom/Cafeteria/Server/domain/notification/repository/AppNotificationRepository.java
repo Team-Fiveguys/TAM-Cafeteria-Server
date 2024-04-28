55cae250e8cd2ff822cd62ad3fce3c93afe12944
wuseong <jws001228@naver.com>
2024-04-28 17:11:25 +0900

    feat: id로 알림 조회하는 기능 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/notification/repository/AppNotificationRepository.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/notification/repository/AppNotificationRepository.java
@@ -3,6 +3,9 @@ package fiveguys.Tom.Cafeteria.Server.domain.notification.repository;
 import fiveguys.Tom.Cafeteria.Server.domain.notification.entity.AppNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppNotificationRepository extends JpaRepository<AppNotification, Long> {
+    public Optional<AppNotification> findById(Long id);
 }
 
