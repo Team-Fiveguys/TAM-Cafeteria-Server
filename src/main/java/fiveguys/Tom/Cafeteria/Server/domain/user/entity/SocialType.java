01f1ba19e010d7c653a77319ebe4849167d1ac55
wuseong <jws001228@naver.com>
2024-05-08 18:19:31 +0900

    feat: 소셜 타입에 이메일 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/user/entity/SocialType.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/user/entity/SocialType.java
@@ -1,7 +1,7 @@
 package fiveguys.Tom.Cafeteria.Server.domain.user.entity;
 
 public enum SocialType {
-    KAKAO, APPLE, GOOGLE;
+    EMAIL, KAKAO, APPLE, GOOGLE;
 
     public static boolean isApple(SocialType socialType) {
         return socialType == APPLE;
