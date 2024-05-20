e2e95e5bd903f2a1413340ed0e9de2b6d74f8ccb
wuseong <jws001228@naver.com>
2024-05-20 20:33:53 +0900

    feat: swagger 설명 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/board/controller/BoardController.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import fiveguys.Tom.Cafeteria.Server.domain.board.dto.*;
 import fiveguys.Tom.Cafeteria.Server.domain.board.entity.Board;
 import fiveguys.Tom.Cafeteria.Server.domain.board.entity.BoardType;
 import fiveguys.Tom.Cafeteria.Server.domain.board.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,7 @@ public class BoardController {
     private BoardService boardService;
 
     // 게시글 생성
+    @Operation(summary = "게시글 생성 API")
     @PostMapping
     public ApiResponse<Board> createBoard(@RequestBody BoardCreateDTO boardCreateDTO) {
         Board board = boardService.createBoard(boardCreateDTO);
@@ -25,12 +27,14 @@ public class BoardController {
     }
 
     // 특정 게시판의 전체 게시글 조회
+    @Operation(summary = "게시글 리스트 조회 API", description = "수정 필요")
     @GetMapping("/{boardType}/boards")
     public List<Board> getAllBoardsByType(@PathVariable("boardType") BoardType boardType) {
         return boardService.getAllBoardsByType(boardType);
     }
 
     //특정 게시글 조회
+    @Operation(summary = "특정 게시글 조회 API")
     @GetMapping("/{id}")
     public ApiResponse<BoardResponseDTO> getBoardById(@PathVariable Long id) {
         BoardResponseDTO boardResponseDTO = boardService.getBoardById(id);
@@ -38,6 +42,7 @@ public class BoardController {
     }
 
     // 게시글 수정
+    @Operation(summary = "특정 게시글 조회 API")
     @PutMapping("/{id}")
     public ApiResponse<BoardResponseDTO> updateBoard(@PathVariable Long id, @RequestBody BoardUpdateDTO boardUpdateDTO) {
         BoardResponseDTO updatedBoard = boardService.updateBoard(id, boardUpdateDTO);
@@ -45,12 +50,14 @@ public class BoardController {
     }
 
     // 게시글 삭제
+    @Operation(summary = "특정 게시글 삭제 API")
     @DeleteMapping("/{id}")
     public void deleteBoard(@PathVariable Long id) {
         boardService.deleteBoard(id);
     }
 
     //게시글 좋아요
+    @Operation(summary = "게시글에 좋아요")
     @PostMapping("/{id}/like")
     public ApiResponse<BoardResponseDTO> toggleLike(@PathVariable Long id, @RequestParam Long userId) {
         BoardResponseDTO boardResponseDTO = boardService.toggleLike(id, userId);
