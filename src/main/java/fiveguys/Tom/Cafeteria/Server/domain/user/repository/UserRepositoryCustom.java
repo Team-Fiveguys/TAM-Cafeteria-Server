509e1b80ed5f03bdcb5f0b620f4e7aa94617caa1
wuseong <jws001228@naver.com>
2024-06-06 21:57:25 +0900

    feat: queryDSL 이용하여 유저 조회하는 메서드 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/user/repository/UserRepositoryCustom.java
@@ -19,5 +19,7 @@ public interface UserRepositoryCustom {
     List<User> findUsersByNotificationSet(String cafeteriaName, AppNotificationType appNotificationType);
 
     List<User> findUsersByNotificationSet(String subscribedCafeteriaName, String unsubscribedCafeteriaName, AppNotificationType appNotificationType);
+
+    List<User> findUsersByNotificationSet(String subscribedCafeteriaName1, AppNotificationType appNotificationType, String subscribedCafeteriaName2);
 }
 
