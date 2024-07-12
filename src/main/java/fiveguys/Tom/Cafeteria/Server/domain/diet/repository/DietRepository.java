40d829c2d121735457fb0152e6e08746c1bc9e87
wuseong <jws001228@naver.com>
2024-07-12 13:27:58 +0900

    feat: 쿼리 튜닝 - 특정 식단 조회 시에 fetch join 적용 - where문 조건으로 객체 사용 시 프록시 객체 이용

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/repository/DietRepository.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/repository/DietRepository.java
@@ -4,6 +4,8 @@ import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Diet;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Meals;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -12,7 +14,18 @@ import java.util.Optional;
 
 public interface DietRepository extends JpaRepository<Diet, Long> {
     List<Diet> findByCafeteriaAndLocalDate(Cafeteria cafeteria, LocalDate localDate);
-    Optional<Diet> findByCafeteriaAndLocalDateAndMeals(Cafeteria cafeteria, LocalDate date, Meals meals);
+
+    @Query("SELECT d FROM Diet d " +
+            "LEFT JOIN FETCH d.menuDietList md " +
+            "LEFT JOIN FETCH md.menu m " +
+            "WHERE d.cafeteria = :cafeteria " +
+            "AND d.localDate = :localDate " +
+            "AND d.meals = :meals")
+    Optional<Diet> findByCafeteriaAndLocalDateAndMealsWithMenuDietAndMenu(
+            @Param("cafeteria") Cafeteria cafeteria,
+            @Param("localDate") LocalDate localDate,
+            @Param("meals") Meals meals
+    );
     List<Diet> findAllByCafeteriaAndYearAndMonthAndWeekAndMeals(Cafeteria cafeteria, int year, int month, int week, Meals meals);
 
     boolean existsByCafeteriaAndLocalDateAndMeals(Cafeteria cafeteria, LocalDate date, Meals meals);
--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryServiceImpl.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryServiceImpl.java
@@ -2,13 +2,12 @@ package fiveguys.Tom.Cafeteria.Server.domain.diet.service;
 
 import fiveguys.Tom.Cafeteria.Server.apiPayload.code.status.ErrorStatus;
 import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
-import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.service.CafeteriaQueryService;
+import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.repository.CafeteriaRepository;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Diet;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Meals;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.repository.DietRepository;
 import fiveguys.Tom.Cafeteria.Server.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -19,7 +18,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class DietQueryServiceImpl implements DietQueryService{
     private final DietRepository dietRepository;
-    private final CafeteriaQueryService cafeteriaQueryService;
+    private final CafeteriaRepository cafeteriaRepository;
 
     @Override
     public Diet getDiet(Long dietId) {
@@ -28,15 +27,15 @@ public class DietQueryServiceImpl implements DietQueryService{
 
     @Override
     public Diet getDiet(Long cafeteriaId, LocalDate localDate, Meals meals) {
-        Cafeteria cafeteria = cafeteriaQueryService.findById(cafeteriaId);
-        Diet diet = dietRepository.findByCafeteriaAndLocalDateAndMeals(cafeteria, localDate, meals)
+        Cafeteria cafeteria = cafeteriaRepository.getReferenceById(cafeteriaId);
+        Diet diet = dietRepository.findByCafeteriaAndLocalDateAndMealsWithMenuDietAndMenu(cafeteria, localDate, meals)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.DIET_NOT_FOUND));
         return diet;
     }
 
     @Override
     public boolean existsDiet(Long cafeteriaId, LocalDate localDate, Meals meals) {
-        Cafeteria cafeteria = cafeteriaQueryService.findById(cafeteriaId);
+        Cafeteria cafeteria = cafeteriaRepository.getReferenceById(cafeteriaId);
         return dietRepository.existsByCafeteriaAndLocalDateAndMeals(cafeteria, localDate, meals);
     }
 
