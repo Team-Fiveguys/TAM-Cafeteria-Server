51508443b7af2fae686dff16bccbc1ea9e2c8e33
wuseong <jws001228@naver.com>
2024-08-27 22:32:51 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryService.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryService.java
@@ -4,17 +4,15 @@ import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Diet;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Meals;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface DietQueryService {
 
-    public Diet getDiet(Long dietId);
     public Diet getDiet(Long cafeteriaId, LocalDate localDate, Meals meals);
 
     public boolean existsDiet(Long cafeteriaId, LocalDate localDate, Meals meals);
-    public List<Diet> getDietsOfDay(Cafeteria cafeteria, LocalDate localDate);
-    public List<Diet> getDietListOfWeek(Cafeteria cafeteria, int year, int month, int weekNum, Meals meals);
+
+    public List<Diet> getThreeWeeksDiet(Long cafeteriaId);
 
 }
--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryServiceImpl.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/service/DietQueryServiceImpl.java
@@ -6,6 +6,7 @@ import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.repository.CafeteriaReposi
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Diet;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Meals;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.repository.DietRepository;
+import fiveguys.Tom.Cafeteria.Server.domain.diet.repository.DietRepositoryCustom;
 import fiveguys.Tom.Cafeteria.Server.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,12 +19,9 @@ import java.util.*;
 @RequiredArgsConstructor
 public class DietQueryServiceImpl implements DietQueryService{
     private final DietRepository dietRepository;
+    private final DietRepositoryCustom dietRepositoryCustom;
     private final CafeteriaRepository cafeteriaRepository;
 
-    @Override
-    public Diet getDiet(Long dietId) {
-        return dietRepository.findById(dietId).orElseThrow(()-> new GeneralException(ErrorStatus.DIET_NOT_FOUND));
-    }
 
     @Override
     public Diet getDiet(Long cafeteriaId, LocalDate localDate, Meals meals) {
@@ -40,16 +38,8 @@ public class DietQueryServiceImpl implements DietQueryService{
     }
 
     @Override
-    public List<Diet> getDietsOfDay(Cafeteria cafeteria, LocalDate localDate) {
-        return dietRepository.findByCafeteriaAndLocalDate(cafeteria, localDate);
+    public List<Diet> getThreeWeeksDiet(Long cafeteriaId) {
+        return dietRepositoryCustom.findDietsByThreeWeeks(cafeteriaId);
     }
 
-    @Override
-    public List<Diet> getDietListOfWeek(Cafeteria cafeteria, int year, int month, int weekNum, Meals meals) {
-        List<Diet> dietList = dietRepository
-                .findAllByCafeteriaAndYearAndMonthAndWeekAndMeals(cafeteria, year, month, weekNum, meals);
-        dietList.stream()
-                .sorted(Comparator.comparing(Diet::getLocalDate));
-        return dietList;
-    }
 }
