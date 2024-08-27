f6b8aee4d8e84e946b84bd9060af9f7622b5043a
wuseong <jws001228@naver.com>
2024-08-27 22:40:15 +0900

    refactor: 식당 주차별 조회 API -> 3주치 통합 API

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/controller/DietController.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/controller/DietController.java
@@ -2,16 +2,13 @@ package fiveguys.Tom.Cafeteria.Server.domain.diet.controller;
 
 
 import fiveguys.Tom.Cafeteria.Server.apiPayload.ApiResponse;
-import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
-import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.repository.CafeteriaRepository;
-import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.service.CafeteriaQueryService;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.converter.DietConverter;
-import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietRequestDTO;
+import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.entity.DietPhoto;
+import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.repository.DietPhotoRepository;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietResponseDTO;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Diet;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.Meals;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.MenuDiet;
-import fiveguys.Tom.Cafeteria.Server.domain.diet.repository.DietRepository;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.service.DietQueryService;
 import fiveguys.Tom.Cafeteria.Server.domain.menu.converter.MenuConverter;
 import fiveguys.Tom.Cafeteria.Server.domain.menu.dto.MenuResponseDTO;
@@ -23,13 +20,14 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/diets")
 public class DietController {
     private final DietQueryService dietQueryService;
-    private final CafeteriaRepository cafeteriaRepository;
+    private final DietPhotoRepository dietPhotoRepository;
     @Value("${cloud.aws.s3.path.prefix}")
     private String prefixURI;
 
@@ -40,34 +38,43 @@ public class DietController {
                                                              @RequestParam(name = "localDate") LocalDate localDate,
                                                              @RequestParam(name = "meals") Meals meals){
         Diet diet = dietQueryService.getDiet(cafeteriaId,localDate, meals);
+        DietPhoto dietPhoto = dietPhotoRepository.findByDiet(diet);
         List<MenuDiet> menuDietList = diet.getMenuDietList();
         List<MenuResponseDTO.MenuQueryDTO> menuList = menuDietList.stream()
                 .map(MenuDiet::getMenu)
                 .map(MenuConverter::toMenuQueryDTO)
                 .collect(Collectors.toList());
-        DietResponseDTO.DietQueryDTO dietQueryResponseDTO = DietConverter.toDietResponseDTO(prefixURI, diet, MenuConverter.toMenuResponseListDTO(menuList));
+        DietResponseDTO.DietQueryDTO dietQueryResponseDTO = DietConverter.toDietResponseDTO(prefixURI, diet, dietPhoto ,MenuConverter.toMenuResponseListDTO(menuList));
         return ApiResponse.onSuccess(dietQueryResponseDTO);
     }
-    @Operation(summary = "식당의 금주의 식단표 API", description = "식당id, 년/월/주차, 식때를 받아서 그 주차의 메뉴 리스트를 반환한다.")
-    @GetMapping("/weeks")
-    public ApiResponse<DietResponseDTO.WeekDietsResponseDTO> getWeekDiets(@RequestParam(name = "cafeteriaId")Long cafeteriaId,
-                                                                          @RequestParam(name = "year") int year,
-                                                                          @RequestParam(name = "month") int month,
-                                                                          @RequestParam(name = "weekNum") int weekNum,
-                                                                          @RequestParam(name = "meals") Meals meals) {
-        Cafeteria cafeteria = cafeteriaRepository.getReferenceById(cafeteriaId);
-        List<Diet> dietListOfWeek = dietQueryService
-                .getDietListOfWeek(cafeteria, year, month, weekNum, meals);
-        List<DietResponseDTO.DietQueryDTO> dietResponseDTOs = dietListOfWeek.stream()
-                .map(diet -> {
-                    List<MenuDiet> menuDietList = diet.getMenuDietList();
-                    List<MenuResponseDTO.MenuQueryDTO> menuList = menuDietList.stream()
-                            .map(MenuDiet::getMenu)
-                            .map(MenuConverter::toMenuQueryDTO)
+    @Operation(summary =  "모든 식당의 3주치의 식단표 API", description = "식당id리스트, 시작년/월/주차, 기간을 입력 받아 메뉴 리스트를 반환한다.")
+    @GetMapping("/main")
+    public ApiResponse<DietResponseDTO.MainViewResponseDTO> getWeekDietsTable(@RequestParam(name = "cafeteriaIdList") List<Long> cafeteriaIdList) {
+        List<DietResponseDTO.ThreeWeeksDietsResponseDTO> threeWeeksDietsResponseDTOS = cafeteriaIdList.stream()
+                .map(id -> dietQueryService.getThreeWeeksDiet(id)) //각 식당 마다
+                .map(dietList -> {
+                    List<DietResponseDTO.DietQueryDTO> dietQueryDTOList = dietList.stream()
+                            .map(diet -> {//각 식단 마다
+                                DietPhoto dietPhoto = dietPhotoRepository.findByDiet(diet);
+                                List<MenuResponseDTO.MenuQueryDTO> menuQueryDTOList = diet.getMenuDietList().stream()
+                                        .map(MenuDiet::getMenu) //각 메뉴 마다
+                                        .map(MenuConverter::toMenuQueryDTO)
+                                        .collect(Collectors.toList());
+                                DietResponseDTO.DietQueryDTO dietQueryResponseDTO = DietConverter.toDietResponseDTO(prefixURI, diet, dietPhoto, MenuConverter.toMenuResponseListDTO(menuQueryDTOList));
+                                return dietQueryResponseDTO;
+                            })
                             .collect(Collectors.toList());
-                    return DietConverter.toDietResponseDTO(prefixURI, diet, MenuConverter.toMenuResponseListDTO(menuList));
+                    return dietQueryDTOList;
                 })
+                .map(dietQueryDTOS -> DietConverter.toThreeWeeksDietsResponseDTO(dietQueryDTOS))
                 .collect(Collectors.toList());
-        return ApiResponse.onSuccess(DietConverter.toWeekDietsResponseDTO(dietResponseDTOs));
+
+        IntStream.range(0, threeWeeksDietsResponseDTOS.size()).forEach(i -> {
+            threeWeeksDietsResponseDTOS.get(i).setCafeteriaId(cafeteriaIdList.get(i));
+        });
+        DietResponseDTO.MainViewResponseDTO responseDTO = DietResponseDTO.MainViewResponseDTO.builder()
+                .threeWeeksDietsResponseDTOS(threeWeeksDietsResponseDTOS)
+                .build();
+        return ApiResponse.onSuccess(responseDTO);
     }
 }
