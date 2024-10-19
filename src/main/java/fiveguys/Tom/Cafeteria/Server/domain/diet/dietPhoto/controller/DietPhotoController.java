4e31a7a7d5eb8f33529e4f2c50d3335014a74f04
wuseong <jws001228@naver.com>
2024-10-19 17:05:25 +0900

    refactor: 식단 사진 업로드/재업로드, 삭제 로직 리팩토링

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/dietPhoto/controller/DietPhotoController.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/dietPhoto/controller/DietPhotoController.java
@@ -2,17 +2,19 @@ package fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.controller;
 
 
 import fiveguys.Tom.Cafeteria.Server.apiPayload.ApiResponse;
+import fiveguys.Tom.Cafeteria.Server.apiPayload.code.status.ErrorStatus;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.converter.DietPhotoConverter;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.dto.DietPhotoResponseDTO;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.entity.DietPhoto;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dietPhoto.service.DietPhotoService;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietRequestDTO;
-import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietResponseDTO;
+import fiveguys.Tom.Cafeteria.Server.exception.GeneralException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 
 
 @RestController
@@ -34,8 +36,10 @@ public class DietPhotoController {
     public ApiResponse<DietPhotoResponseDTO.DietPhotoUploadDTO> updateDietPhoto(
             @RequestPart(name = "photo")MultipartFile multipartFile,
             @RequestPart(name = "dietQuery") DietRequestDTO.DietQueryDTO dietQueryDTO){
-        dietPhotoService.deleteDietPhoto(dietQueryDTO);
-        DietPhoto dietPhoto = dietPhotoService.uploadDietPhoto(dietQueryDTO, multipartFile);
+        DietPhoto previousDietPhoto = dietPhotoService.getDietPhoto(dietQueryDTO).orElseThrow(() -> new GeneralException(ErrorStatus.DIET_PHOTO_NOT_FOUND));
+        String previousImageKey = previousDietPhoto.getImageKey();
+        DietPhoto dietPhoto = dietPhotoService.reuploadDietPhoto(dietQueryDTO, multipartFile);
+        dietPhotoService.deleteFile(previousImageKey);
         return ApiResponse.onSuccess(DietPhotoConverter.toDietPhotoUploadDTO(dietPhoto));
     }
 }
--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/dietPhoto/service/DietPhotoService.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/diet/dietPhoto/service/DietPhotoService.java
@@ -5,9 +5,14 @@ import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietRequestDTO;
 import fiveguys.Tom.Cafeteria.Server.domain.diet.dto.DietResponseDTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 public interface DietPhotoService {
+    public Optional<DietPhoto> getDietPhoto(DietRequestDTO.DietQueryDTO dietQueryDTO);
 
     public DietPhoto uploadDietPhoto(DietRequestDTO.DietQueryDTO dietQueryDTO, MultipartFile multipartFile);
 
-    public DietPhoto deleteDietPhoto(DietRequestDTO.DietQueryDTO dietQueryDTO);
+    public DietPhoto reuploadDietPhoto(DietRequestDTO.DietQueryDTO dietQueryDTO, MultipartFile multipartFile);
+
+    public void deleteFile(String fileKey);
 }
